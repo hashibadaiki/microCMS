50df28a73a5dd1b7c8e44f8dd1f1397d8bf23f1a
hashibadaiki <higemeganeking@gmail.com>
2021-04-25 12:28:33 +0900

    全てのファイルに目を通してざっと説明書きまで完了した

--- plugins/vue-scrollto.js
+++ plugins/vue-scrollto.js
@@ -3,6 +3,9 @@ import VueScrollTo from 'vue-scrollto';
 
 Vue.use(VueScrollTo);
 
+// 恐らくpageTopに戻るみたいなpluginっぽい
+// TODO: 要確認
+
 export default function vueScrollTo(context, inject) {
   inject('scrollTo', VueScrollTo.scrollTo);
 }
